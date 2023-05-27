b6aa7ce87588e1654a2da821d180f1b4691361de
Valérian M <valerian.mermoz@gmail.com>
2023-05-27 19:06:09 +0200

    commit
    commit

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,54 +1,87 @@
 // Get the modal
 const modal = document.getElementById("myModal");
 const galleryModale = document.querySelector(".modalImg");
+const modalFooter = document.querySelector(".modalFooter");
+const modalContent = document.getElementById("modal-content");
+const modalContentDeux = document.querySelector(".modal-content2");
+const flecheRetour = document.querySelector("fa-solid fa-arrow-left")
+
+
 
 // Get the button that opens the modal
 const btn = document.getElementById("buttonEdition");
 const btnmodif = document.getElementById("buttonmodif");
-const btnmodif2 = document.getElementById("buttonmodif2")
+const btnmodif2 = document.getElementById("buttonmodif2");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-btnmodif.onclick = function() {
+btnmodif.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-btnmodif2.onclick = function() {
+btnmodif2.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-
+};
 
 function modaleGallery(data) {
   for (let i = 0; i < data.length; i++) {
-    const divOfImg = document.createElement("div");
+    const divOfImg = document.createElement("figure");
     const imageDiv = document.createElement("img");
     imageDiv.src = data[i].imageUrl;
+
+    const poubelle = document.createElement("i");
+    poubelle.setAttribute("class", "fa-solid fa-trash-can");
 
     const figCaption = document.createElement("figcaption");
     figCaption.textContent = "éditer";
 
+
+
     divOfImg.appendChild(imageDiv);
     divOfImg.appendChild(figCaption);
+    divOfImg.appendChild(poubelle);
     galleryModale.appendChild(divOfImg);
   }
+
+  const boutonAjout = document.createElement("button");
+  boutonAjout.setAttribute("class", "boutonajout");
+  boutonAjout.setAttribute("id", "Ajout");
+  boutonAjout.textContent = "Ajouter une photo";
+  boutonAjout.addEventListener("click", function() {
+    modalContent.style.display = "none";
+    modalContentDeux.style.display = "flex";
+    });
+
+  const supprGalerie = document.createElement("p");
+  supprGalerie.textContent = "Supprimer la galerie"
+
+  modalFooter.appendChild(boutonAjout);
+  modalFooter.appendChild(supprGalerie);
+
+}
+if (flecheRetour){
+flecheRetour.addEventListener("click", function() {
+  modalContent.style.display = "flex";
+  modalContentDeux.style.display = "none";
+  });
 }
 
 //Recuperation des projets depuis l'api fourni par le backend
@@ -80,6 +113,4 @@ async function initModale() {
   await generateModaleGallery();
 }
 
-
 initModale();
-
